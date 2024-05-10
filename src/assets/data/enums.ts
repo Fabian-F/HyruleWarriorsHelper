393b3ce6f381746251c71674717e5c874fa15fd9
Fabian Forster <fabianforster@hotmail.de>
2024-05-10 09:52:08 +0200

    Added food and skill data

--- src/assets/data/enums.ts
+++ src/assets/data/enums.ts
@@ -21,3 +21,34 @@ export enum Difficulty {
   BLUE = "Blue",
   RED = "Red"
 }
+
+export enum Element {
+  LIGHT,
+  DARKNESS,
+  FIRE,
+  WATER,
+  LIGHTNING
+}
+
+export enum FoodCategory {
+  DRINK,
+  FEAST,
+  PLANT,
+  WEIRD,
+  FISH
+}
+
+export enum FairySkillGrade {
+  BRONZE,
+  SILVER,
+  GOLD,
+  RAINBOW
+}
+
+export enum FairySkillType {
+  ATTACK,
+  DEFENSE,
+  SUPPORT,
+  MOVEMENT,
+  REGAIN
+}
---
+++ src/assets/data/my-fairy/fairy-skills.data.ts
@@ -0,0 +1,419 @@
+import { FairySkill } from "src/app/models"
+import { FairySkillGrade, FairySkillType } from "../enums"
+
+export const Rupee_Master: FairySkill = {
+  name: "Rupee Master",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.SUPPORT,
+  stats: {
+    friendly: 100,
+    fleet_shrewd: 100,
+    soft_relaxed: 125,
+  },
+}
+
+export const Wall_of_Water: FairySkill = {
+  name: "Wall of Water",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.DEFENSE,
+  stats: {
+    sparkly: 100,
+    flashy_valiant: 100,
+    smiley: 125,
+  },
+}
+
+export const XP_Master: FairySkill = {
+  name: "XP Master",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.SUPPORT,
+  stats: {
+    resolute: 100,
+    dizzy: 125,
+    eager: 100,
+  },
+}
+
+export const Dream_Sphere: FairySkill = {
+  name: "Dream Sphere",
+  grade: FairySkillGrade.BRONZE,
+  type: FairySkillType.DEFENSE,
+  stats: {
+    flashy_valiant: 50,
+    smiley: 50,
+  },
+}
+
+export const Elemental_Magician: FairySkill = {
+  name: "Elemental Magician",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.ATTACK,
+  stats: {
+    eager: 100,
+    dreamy_aspiring: 125,
+    soft_relaxed: 100,
+  },
+}
+
+export const Extreme_Crush: FairySkill = {
+  name: "Extreme Crush",
+  grade: FairySkillGrade.GOLD,
+  type: FairySkillType.ATTACK,
+  stats: {
+    sparkly: 200,
+    friendly: 200,
+    flashy_valiant: 225,
+    smiley: 225,
+  },
+}
+
+export const Extreme_Crush_Plus: FairySkill = {
+  name: "Extreme Crush+",
+  grade: FairySkillGrade.RAINBOW,
+  type: FairySkillType.ATTACK,
+  stats: {
+    sparkly: 225,
+    friendly: 225,
+    resolute: 200,
+    flashy_valiant: 225,
+    smiley: 255,
+  },
+}
+
+export const Extreme_Evasion: FairySkill = {
+  name: "Extreme Evasion",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.DEFENSE,
+  stats: {
+    sparkly: 125,
+    eager: 100,
+    flashy_valiant: 100,
+  },
+}
+
+export const Extreme_Gusts: FairySkill = {
+  name: "Extreme Gusts",
+  grade: FairySkillGrade.BRONZE,
+  type: FairySkillType.MOVEMENT,
+  stats: {
+    friendly: 75,
+    flashy_valiant: 50,
+  },
+}
+
+export const Fill_Magic: FairySkill = {
+  name: "Fill Magic",
+  grade: FairySkillGrade.BRONZE,
+  type: FairySkillType.REGAIN,
+  stats: {
+    sparkly: 50,
+    dizzy: 75,
+  },
+}
+
+export const Fill_Special_Attack: FairySkill = {
+  name: "Fill Special Attack",
+  grade: FairySkillGrade.BRONZE,
+  type: FairySkillType.REGAIN,
+  stats: {
+    sparkly: 50,
+    flashy_valiant: 75,
+  },
+}
+
+export const Glass_Cannon: FairySkill = {
+  name: "Glass Cannon",
+  grade: FairySkillGrade.GOLD,
+  type: FairySkillType.ATTACK,
+  stats: {
+    sparkly: 200,
+    friendly: 225,
+    resolute: 200,
+    dizzy: 225,
+  },
+}
+
+export const Hard_Worker: FairySkill = {
+  name: "Hard Worker",
+  grade: FairySkillGrade.BRONZE,
+  type: FairySkillType.REGAIN,
+  stats: {
+    sparkly: 75,
+    resolute: 50,
+  },
+}
+
+export const Health_Regain: FairySkill = {
+  name: "Health Regain",
+  grade: FairySkillGrade.BRONZE,
+  type: FairySkillType.REGAIN,
+  stats: {
+    resolute: 75,
+    eager: 50,
+  },
+}
+
+export const Health_Sponge: FairySkill = {
+  name: "Health Sponge",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.ATTACK,
+  stats: {
+    sparkly: 125,
+    friendly: 100,
+    dizzy: 100,
+  },
+}
+
+export const Heart_Bonds: FairySkill = {
+  name: "Heart Bonds",
+  grade: FairySkillGrade.BRONZE,
+  type: FairySkillType.REGAIN,
+  stats: {
+    friendly: 50,
+    eager: 75,
+  },
+}
+
+export const Invincible_Weakling: FairySkill = {
+  name: "Invincible Weakling",
+  grade: FairySkillGrade.BRONZE,
+  type: FairySkillType.DEFENSE,
+  stats: {
+    resolute: 75,
+    smiley: 50,
+  },
+}
+
+export const Keep_Keeper: FairySkill = {
+  name: "Keep Keeper",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.ATTACK,
+  stats: {
+    resolute: 100,
+    flashy_valiant: 125,
+    smiley: 100,
+  },
+}
+
+export const Magic_Bonds: FairySkill = {
+  name: "Magic Bonds",
+  grade: FairySkillGrade.BRONZE,
+  type: FairySkillType.REGAIN,
+  stats: {
+    dizzy: 75,
+    eager: 50,
+  },
+}
+
+export const Magic_Fountain: FairySkill = {
+  name: "Magic Fountain",
+  grade: FairySkillGrade.GOLD,
+  type: FairySkillType.ATTACK,
+  stats: {
+    eager: 225,
+    flashy_valiant: 200,
+    fleet_shrewd: 225,
+    soft_relaxed: 200,
+  },
+}
+
+export const Magic_Fountain_Plus: FairySkill = {
+  name: "Magic Fountain+",
+  grade: FairySkillGrade.RAINBOW,
+  type: FairySkillType.ATTACK,
+  stats: {
+    eager: 225,
+    flashy_valiant: 225,
+    dreamy_aspiring: 200,
+    fleet_shrewd: 255,
+    soft_relaxed: 225,
+  },
+}
+
+export const Magic_Sponge: FairySkill = {
+  name: "Magic Sponge",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.ATTACK,
+  stats: {
+    eager: 100,
+    fleet_shrewd: 125,
+    soft_relaxed: 100,
+  },
+}
+
+export const Marathon_Runner: FairySkill = {
+  name: "Marathon Runner",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.MOVEMENT,
+  stats: {
+    sparkly: 100,
+    friendly: 125,
+    smiley: 100,
+  },
+}
+
+export const Material_Master: FairySkill = {
+  name: "Material Master",
+  grade: FairySkillGrade.GOLD,
+  type: FairySkillType.SUPPORT,
+  stats: {
+    resolute: 225,
+    flashy_valiant: 200,
+    dreamy_aspiring: 225,
+    soft_relaxed: 200,
+  },
+}
+
+export const Repair_Technician: FairySkill = {
+  name: "Repair Technician",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.DEFENSE,
+  stats: {
+    sparkly: 100,
+    friendly: 100,
+    resolute: 125,
+  },
+}
+
+export const Special_Bonds: FairySkill = {
+  name: "Special Bonds",
+  grade: FairySkillGrade.BRONZE,
+  type: FairySkillType.REGAIN,
+  stats: {
+    friendly: 50,
+    smiley: 75,
+  },
+}
+
+export const Special_Fountain: FairySkill = {
+  name: "Special Fountain",
+  grade: FairySkillGrade.GOLD,
+  type: FairySkillType.ATTACK,
+  stats: {
+    sparkly: 225,
+    resolute: 200,
+    eager: 200,
+    flashy_valiant: 225,
+  },
+}
+
+export const Special_Fountain_Plus: FairySkill = {
+  name: "Special Fountain+",
+  grade: FairySkillGrade.RAINBOW,
+  type: FairySkillType.ATTACK,
+  stats: {
+    sparkly: 255,
+    resolute: 225,
+    eager: 225,
+    flashy_valiant: 225,
+    dreamy_aspiring: 200,
+  },
+}
+
+export const Special_Regain: FairySkill = {
+  name: "Special Regain",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.REGAIN,
+  stats: {
+    friendly: 100,
+    resolute: 100,
+    smiley: 125,
+  },
+}
+
+export const Special_Sponge: FairySkill = {
+  name: "Special Sponge",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.ATTACK,
+  stats: {
+    flashy_valiant: 100,
+    dreamy_aspiring: 100,
+    soft_relaxed: 125,
+  },
+}
+
+export const Total_Focus: FairySkill = {
+  name: "Total Focus",
+  grade: FairySkillGrade.SILVER,
+  type: FairySkillType.REGAIN,
+  stats: {
+    friendly: 100,
+    resolute: 100,
+    eager: 125,
+  },
+}
+
+export const Food_Master_Plus: FairySkill = {
+  name: "Food Master+",
+  grade: FairySkillGrade.RAINBOW,
+  type: FairySkillType.SUPPORT,
+  stats: {
+    resolute: 225,
+    eager: 255,
+    flashy_valiant: 200,
+    dreamy_aspiring: 225,
+    soft_relaxed: 225,
+  },
+}
+
+export const Rupee_Master_Plus: FairySkill = {
+  name: "Rupee Master+",
+  grade: FairySkillGrade.GOLD,
+  type: FairySkillType.SUPPORT,
+  stats: {
+    friendly: 200,
+    dizzy: 200,
+    fleet_shrewd: 200,
+    soft_relaxed: 225,
+  },
+}
+
+export const Wall_of_water_Plus: FairySkill = {
+  name: "Wall of water+",
+  grade: FairySkillGrade.GOLD,
+  type: FairySkillType.DEFENSE,
+  stats: {
+    sparkly: 225,
+    flashy_valiant: 200,
+    smiley: 225,
+    dreamy_aspiring: 200,
+  },
+}
+
+export const XP_Master_Plus: FairySkill = {
+  name: "XP Master+",
+  grade: FairySkillGrade.GOLD,
+  type: FairySkillType.SUPPORT,
+  stats: {
+    sparkly: 200,
+    resolute: 225,
+    dizzy: 225,
+    eager: 200,
+  },
+}
+
+export const Weapon_Master: FairySkill = {
+  name: "Weapon Master",
+  grade: FairySkillGrade.GOLD,
+  type: FairySkillType.SUPPORT,
+  stats: {
+    friendly: 225,
+    eager: 200,
+    fleet_shrewd: 225,
+    soft_relaxed: 200,
+  },
+}
+
+export const Weapon_Master_Plus: FairySkill = {
+  name: "Weapon Master+",
+  grade: FairySkillGrade.RAINBOW,
+  type: FairySkillType.SUPPORT,
+  stats: {
+    friendly: 225,
+    dizzy: 200,
+    eager: 225,
+    fleet_shrewd: 225,
+    soft_relaxed: 225,
+  },
+}
+
