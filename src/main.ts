1f2ce32515dde1cfadc93f9e08a4f691bacfdcad
fabian-f <fabianforster@hotmail.de>
2026-09-22 22:59:20 +0200

    [v2] fix 404

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+const redirect = sessionStorage.getItem('github-pages-redirect');
+
+if (redirect) {
+  sessionStorage.removeItem('github-pages-redirect');
+
+  const { route, search, hash } = JSON.parse(redirect);
+
+  window.history.replaceState(null, '', route + search + hash);
+}
+
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
