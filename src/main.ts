972cfc632fe709fe1eac51ec8c0d48059226a14e
fabian-f <fabianforster@hotmail.de>
2026-09-22 23:06:53 +0200

    [v2] fix 404

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,9 @@ const redirect = sessionStorage.getItem('github-pages-redirect');
 
 if (redirect) {
   sessionStorage.removeItem('github-pages-redirect');
-
-  const { route, search, hash } = JSON.parse(redirect);
-
-  window.history.replaceState(null, '', route + search + hash);
+  window.history.replaceState(null, '', redirect);
 }
+
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
