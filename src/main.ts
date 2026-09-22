d3a490133a6ca71b31775601c3a8dc48321a0bb9
fabian-f <fabianforster@hotmail.de>
2026-09-22 23:08:17 +0200

    fix 404

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+const redirect = sessionStorage.getItem('github-pages-redirect');
+
+if (redirect) {
+  sessionStorage.removeItem('github-pages-redirect');
+  window.history.replaceState(null, '', redirect);
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.error(err));
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
